c36d6a0c29f21681a136173df9f4e203f79f6018
razaiqbal07 <mailrazaiqbal@gmail.com>
2026-06-16 16:50:48 +0400

    tuned logic

--- app/services/chat.py
+++ app/services/chat.py
@@ -3,18 +3,19 @@ from app.services.llm import get_llm_response
 from app.services.memory_extractor import extract_memory
 from app.services.memory import add
 
+MATCH_SCORE_THRESHOLD=0.3
+
 def respond(message:str):
     memory=extract_memory(message)
     # add message to the memory if it needs to be saved
     if memory['shouldSave']:
-        add(message);
-        response=get_llm_response(message,[])
-    else:
-        # process message as a query
-        search_result=query(message)
-        filtered_match=[result for result in search_result.points if result.score > 0.3]
-        memories = [search.payload['text'] for search in filtered_match]
-        if len(memories) < 1:
-            return "I don't have relevant memory to answer this"
-        response=get_llm_response(message,memories)
+        add(memory['message']);
+
+    # process message as a query
+    search_result=query(message)
+    filtered_match=[result for result in search_result.points if result.score > MATCH_SCORE_THRESHOLD]
+    memories = [search.payload['text'] for search in filtered_match]
+    # if len(memories) < 1:
+    #     return "I don't have relevant memory to answer this"
+    response=get_llm_response(message,memories)
     return response
